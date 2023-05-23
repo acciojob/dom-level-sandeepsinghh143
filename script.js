1b203630d87f9046b4644e1deacebdb8ed858ce3
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-23 15:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var element = document.getElementById("level");
+  var level = 0;
+
+  while (element && element !== document.body) {
+    element = element.parentNode;
+    level++;
+  }
+
+  return level;
+alert("The level of the element is:""+level);
